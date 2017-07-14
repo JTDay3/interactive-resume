83b27b736d6256abd3a69a50738e37b21f805441
JTDay3 <JTDay3@mail.com>
2017-07-13 17:27:47 -0700

    Add files via upload

---
+++ js/resumeBuilder.js
@@ -0,0 +1,357 @@
+var bio = {
+    "name": "John T. Day III",
+    "role": "Front-End Developer",
+    "contacts": {
+        "mobile": "480.351.9610",
+        "email": "JTDay3@mail.com",
+        "github": "JTDay3",
+        "twitter": "@JT_Day3",
+        "location": "Scottsdale, Arizona"
+    },
+    "skills": ["HTML5","CSS3", "Responsive Design", "Google Analytics", "Google AdWords", "JavaScript", "jQuery", "Bootstrap", "JSON", "AJAX", "Website Optimization", "Jasmine", "Backbone"],
+    "biopic": "images/Jake.jpg"
+};
+
+var education = {
+    "schools": [{
+            "name": "Udacity",
+            "dates": "December 2016 - Present",
+            "location": "Mountain View, California",
+            "degree": "Front-End Developer Nanodegree",
+            "majors": ["Software Development"],
+            "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+        },
+
+        {
+            "name":"Google Analytics Academy",
+            "dates":"June 2017",
+            "location":"Mountain View, California",
+            "degree":"Google Analytics Certification",
+            "majors":"Google Analytics",
+            "url":"https://analytics.google.com/analytics/academy/"
+        },
+
+        {
+            "name":"Google: Academy for Ads",
+            "dates":"July 2017",
+            "location":"Mountain View, California",
+            "degree":"Google AdWords Certification",
+            "majors":"Google AdWords",
+            "url":"https://support.google.com/partners/answer/3154326?hl=en"
+        },
+
+        {
+            "name": "Arizona State University",
+            "dates": "December 2014",
+            "location": "Tempe, Arizona",
+            "degree": "Bachelor of Arts",
+            "majors": ["Political Science"],
+            "url": "www.asu.edu"
+        }
+    ],
+    "onlineCourses": [{
+            "school": "Udacity",
+            "title": "Intro to HTML and CSS",
+            "dates": "December 2016",
+            "url": "https://www.udacity.com/course/intro-to-html-and-css--ud304"
+        },
+        {
+            "school": "Udacity",
+            "title": "Responsive Web Design Fundamentals",
+            "dates": "December 2016",
+            "url": "https://www.udacity.com/course/responsive-web-design-fundamentals--ud893"
+        },
+        {
+            "school": "Udacity",
+            "title": "Responsive Images",
+            "dates": "January 2017",
+            "url": "https://www.udacity.com/course/responsive-images--ud882"
+        },
+        {
+            "school": "Udacity",
+            "title": "JavaScript Basics",
+            "dates": "March 2017",
+            "url": "https://www.udacity.com/course/javascript-basics--ud804"
+        },
+        {
+            "school": "Udacity",
+            "title": "Intro to jQuery",
+            "dates": "April 2017",
+            "url": "https://www.udacity.com/course/javascript-basics--ud804"
+        },
+        {
+            "school": "Udacity",
+            "title": "Object-Oriented JavaScript",
+            "dates": "May 2017",
+            "url": "https://www.udacity.com/course/object-oriented-javascript--ud015"
+        },
+        {
+            "school":"Google Analytics Academy",
+            "title":"Google Analytics for Beginners",
+            "dates":"June 2017",
+            "url":"https://analytics.google.com/analytics/academy/course/6"
+        },
+
+        {
+            "school":"Google Analytics Academy",
+            "title":"Advanced Google Analytics",
+            "dates":"June 2017",
+            "url":"https://analytics.google.com/analytics/academy/course/7"
+        },
+
+        {
+            "school": "Udacity",
+            "title": "HTML5 Canvas",
+            "dates": "June 2017",
+            "url": "https://www.udacity.com/course/html5-canvas--ud292"
+
+        },
+        {
+            "school": "Udacity",
+            "title": "Browser Rendering Optimization",
+            "dates": "June 2017",
+            "url": "https://www.udacity.com/course/browser-rendering-optimization--ud860"
+        },
+        {
+            "school": "Udacity",
+            "title": "Website Performance Optimization",
+            "dates": "June 2017",
+            "url": "https://www.udacity.com/course/website-performance-optimization--ud884"
+        },
+        {
+            "school": "Udacity",
+            "title": "Intro to AJAX",
+            "dates": "June 2017",
+            "url": "https://www.udacity.com/course/intro-to-ajax--ud110"
+
+        },
+        {
+            "school":"Google: Academy for Ads",
+            "title":"AdWords Fundamentals",
+            "dates":"July 2017",
+            "url":"https://landing.google.com/academyforads/#?modal_active=none"
+        },
+
+        {
+            "school":"Google: Academy for Ads",
+            "title":"AdWords Search Basics",
+            "dates":"July 2017",
+            "url":"https://academy.exceedlms.com/student/home/show_enrollment/2128936#"
+        },
+
+        {
+            "school": "Udacity",
+            "title": "JavaScript Design Patterns",
+            "dates": "July 2017",
+            "url": "https://www.udacity.com/course/javascript-design-patterns--ud989"
+        },
+        {
+            "school": "Udacity",
+            "title": "JavaScript Testing",
+            "dates": "July 2017",
+            "url": "https://www.udacity.com/course/javascript-testing--ud549"
+        },
+    ]
+};
+
+var work = {
+    "jobs": [{
+        "employer": "Postmates",
+        "location": "Phoenix, Arizona",
+        "title": "Courier",
+        "dates": "May 2016 - Present",
+        "description": " Any product within the greater Phoenix area in under one hour, being punctual, knowledgable of Phoenix area, attention to detail, customer experience."
+    }, {
+        "employer": "The Vanguard Group",
+        "location": "Scottsdale, Arizona",
+        "title": "Client Relationship Specialist",
+        "dates": "October 2015 - May 2016",
+        "description": "Assisting all types of investors with meeting their financial goals-the young adult opening a first account, a family saving for a child’s college education, a married couple investing for their first home, a high net worth client preserving her wealth, or an entrepreneur managing his small-business retirement plan. Developing relationships with clients to understand their unique investment needs and position appropriate products or solutions.",
+    }, {
+        "employer": "Snake Eyes Pictures",
+        "location": "Santa Monica, California",
+        "title": "Associate Producer",
+        "dates": "March 2014 - Present",
+        "description": "Served as an associate producer for the soon to be released feature-film, 'UZLA'. Often referred to as the 'AP', an Associate Producer generally assists the producer in putting the TV program or film together. Duties may include writing, editing, organizing scripts. Primary responsibility was cultivate relationships with businesses such as Red Bull, Adidas, University of California, Los Angeles, Deep Eddy Vodka, Hooters, etc. and to serve as the lead for product placement and/or sponsorships. ",
+
+    }, {
+        "employer": "Goldwater Institute",
+        "location": "Phoenix, Arizona",
+        "title": "Ronald Reagan Fellow",
+        "dates": "August 2012 - February 2013",
+        "description": "Through policy research, staff mentoring, seminar attendance, event participation and administrative assistance, Ronald Reagan Fellows gain a foundational understanding of the classical liberal principles that shaped Reagan's presidency. They also gain first-hand knowledge of how those principles relate to current public policy, and how organizations like the Goldwater Institute shape public policy at both local and national levels. Fellows are selected each fall, spring and summer semesters. A minimum of 24 hours per week is required during the spring and fall semesters. All summer interns are expected to work 40 hours per week.",
+
+    }, {
+        "employer": "Harvard University",
+        "location": "Cambridge, Massachusetts",
+        "title": "Athletics Intern",
+        "dates": "May 2012 - August 2012",
+        "description": "Worked directly with selected teams to effectively promote and publicize the department of athletics through campus, local and national media outlets, as well as through the GoCrimson.com website and social media avenues. Also served as a writer and editor for the department's website as well as for game programs and other printed pieces. Gained hands-on experience at a number of athletic events. Also produced video feature and highlight content for YouTube, Facebook, Twitter and GoCrimson.com.",
+
+    }, {
+        "employer": "Arizona State University",
+        "location": "Tempe, Arizona",
+        "title": "Grounds Crew",
+        "dates": "April 2009 - April 2012",
+        "description": "Duties include: assist Facility Maintenance Coordinator with the daily maintenance, practice set up and event set up of all athletic facilities; on-duty during sporting events."
+    }]
+};
+
+var projects = {
+    "projects": [{
+            "title": "Portfolio Page",
+            "dates": "February 2017",
+            "description": "You will be provided with a design mockup as a PDF-file and must replicate that design in HTML and CSS. You will develop a responsive website that will display images, descriptions and links to each of the portfolio projects you will complete throughout the course of the Front-End Web Developer Nanodegree.",
+            "images":["images/portfolio.jpeg"]
+        },
+
+        {
+            "title": "Interactive Resume",
+            "dates": "April 2017",
+            "description": "You will develop an interactive resume application that reads your resume content from a JSON file and dynamically displays that content within a provided template. You will use objects, functions, conditionals, and control structures to compose the content that will display on the resume.",
+            "images":["images/resume.jpeg"]
+        }, {
+            "title": "Classic Arcade Game Clone",
+            "dates": "July 2017",
+            "description": "You will be provided with visual assets and a game loop engine; using these tools you must add a number of entities to the game including the player characters and enemies to recreate the classic arcade game Frogger.",
+            "images":["images/frogger2.png"]
+           
+        }, {
+            "title": "Website Optimization",
+            "dates": "July 2017",
+            "description": "You will optimize a provided website with a number of optimization- and performance-related issues so that it achieves a target PageSpeed score and runs at 60 frames per second.",
+            "images":["images/healthy2.jpeg"]
+            
+        }, {
+            "title": "Neighborhood Map",
+            "dates": "August 2017",
+            "description": "You will develop a single-page application featuring a map of your neighborhood or a neighborhood you would like to visit. You will then add additional functionality to this application, including: map markers to identify popular locations or places you’d like to visit, a search function to easily discover these locations, and a listview to support simple browsing of all locations. You will then research and implement third-party APIs that provide additional information about each of these locations (such as StreetView images, Wikipedia articles, Yelp reviews, etc).",
+            "images":["images/map.jpg"]
+        },{
+            "title": "Feed Reader Testing",
+            "dates": "August 2017",
+            "description": "In this project you are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they’ve already included Jasmine and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we’re now left with an application with an incomplete test suite. That’s where you come in.",
+            "images":["images/testing.jpeg"]
+        }
+    ]
+};
+
+
+bio.display = function() {
+
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").prepend(formattedRole);
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    $("#header").prepend(formattedName);
+    var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+    $("#header").append(formattedBioPic);
+
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    $("#topContacts").append(formattedMobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    $("#topContacts").append(formattedEmail);
+    var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+    $("#topContacts").append(formattedGitHub);
+    var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+    $("#topContacts").append(formattedTwitter);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#topContacts").append(formattedLocation);
+    $("#footerContacts").append(formattedMobile + formattedEmail + formattedGitHub + formattedTwitter + formattedLocation);
+
+    if (bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
+
+        for (var b = 0; b < bio.skills.length; b++) {
+            $("#skills").append(HTMLskills.replace("%data%", bio.skills[b]));
+        }
+    }
+};
+
+
+education.display = function() {
+
+    if (education.schools.length > 0 || education.onlineCourses.length > 0) {
+        for (var i = 0; i < education.schools.length; i++) {
+            $("#education").append(HTMLschoolStart);
+
+            var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+            var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+            var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+            var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
+
+            $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+            $(".education-entry:last").append(formattedSchoolDates);
+            $(".education-entry:last").append(formattedSchoolLocation);
+            $(".education-entry:last").append(formattedSchoolMajor);
+            $("#education").append("<br></br>");
+        }
+
+        if (education.onlineCourses.length > 0) {
+            $("#education").append(HTMLonlineClasses);
+            for (var e = 0; e < education.onlineCourses.length; e++) {
+                $("#education").append(HTMLschoolStart);
+                var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[e].title).replace("#", education.onlineCourses[e].url);
+                var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[e].school);
+                var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[e].dates);
+
+                $(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+                $(".education-entry:last").append(formattedOnlineDates);
+                $("#education").append("<br></br>");
+
+            }
+        }
+
+    }
+};
+
+
+work.display = function() {
+    for (var j = 0; j < work.jobs.length; j++) {
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[j].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[j].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[j].dates);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[j].location);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[j].description);
+
+        $(".work-entry:last").append(formattedEmployerTitle);
+        $(".work-entry:last").append(formattedDates);
+        $(".work-entry:last").append(formattedLocation);
+        $(".work-entry:last").append(formattedDescription);
+        $(".work-entry:last").append("<br>");
+    }
+};
+
+
+
+projects.display = function() {
+    for (var p = 0; p < projects.projects.length; p++) {
+        $("#projects").append(HTMLprojectStart);
+
+    for (var m = 0; m < projects.projects[p].images.length; m++){
+        $(".project-entry:last").append(formattedProjectImage);
+
+        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[p].title);
+        var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[p].dates);
+        var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[p].description);
+        var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[p].images);
+        $(".project-entry:last").append(formattedProjectTitle);
+        $(".project-entry:last").append(formattedProjectDates);
+        $(".project-entry:last").append(formattedProjectDescription);
+        $(".project-entry:last").append("<br>");
+    }
+}
+};
+
+
+bio.display();
+education.display();
+work.display();
+projects.display();
+
+$("#mapDiv").append(googleMap);
+
+
